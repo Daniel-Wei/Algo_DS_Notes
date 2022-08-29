9661348dd377160b6a52879c24ec999a220af040
Daniel-Wei <49779031+Daniel-Wei@users.noreply.github.com>
2022-08-29 19:05:47 +1000

    Create Solution.py

---
+++ 算法/String & Sort/937-Reorder_Data_in_Log_Files/Solution.py
@@ -0,0 +1,18 @@
+class Solution:
+    def reorderLogFiles(self, logs: List[str]) -> List[str]:
+        lets = []
+        digs = []
+    
+        
+        for log in logs:
+            
+            # 将digit-logs按顺序存放
+            if log[-1].isdigit():
+                digs += [log]
+            else:
+                lets +=[log]
+                
+        # 先sort letter-logs的内容， 再sort identifiers    
+        lets.sort(key = lambda x: (x.split()[1:],  x.split()[0]))
+    
+        return lets + digs
