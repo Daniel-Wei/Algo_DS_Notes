b2bac994cbcda60e863b8ed6ffb9c6fa70365e05
Daniel-Wei <49779031+Daniel-Wei@users.noreply.github.com>
2022-08-30 16:40:26 +1000

    Create Solution.py

---
+++ 数据结构/hashmap/692-Top_K_Frequent_Words/Solution.py
@@ -0,0 +1,34 @@
+class Solution:
+    def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        freq = {}
+        
+        for word in words:
+            if word in freq:
+                freq[word] += 1
+            else:
+                freq[word] = 1
+                
+# 不能使用heapq 的 nlargest， 其他相同freq但是lexicograpgical order更小的可能被忽略  
+              
+#          for word in set(words):
+#             heapq.heappush(h, (freq[word], word))
+            
+#         res = heapq.nlargest(k, h)
+#         res = sorted(res, key = lambda x : x[1])
+#         res = sorted(res, key = lambda x : x[0], reverse = True) 
+        
+#         print(res)
+
+
+        res = []
+
+        for word in set(words):
+            res += [[word, freq[word]]]
+        
+        
+#       因为最终的排列顺序是先freq再lexicographical order
+#       所以sorted的使用顺序是先lexicographical order再freq
+        res = sorted(res, key = lambda x : x[0])
+        res = sorted(res, key = lambda x: x[1], reverse = True)
+        
+        return [item[0] for item in res[:k]]
