687da11f7eca1b84e6d435dcefb4b16c66955109
Daniel-Wei <49779031+Daniel-Wei@users.noreply.github.com>
2022-08-30 16:15:17 +1000

    Create Solution.py

---
+++ Maths/48-Rotate_Image/Solution.py
@@ -0,0 +1,17 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        
+        ### reverse rows
+        n = len(matrix)
+        matrix.reverse()
+        
+        
+        ### swap symmetry
+        for x in range(n):
+            for y in range(x):
+                matrix[x][y], matrix[y][x] = matrix[y][x], matrix[x][y]
+        
+        return matrix
