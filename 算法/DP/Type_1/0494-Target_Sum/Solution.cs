c5bf4448da1cb4e119a540aba4081d54745461e3
Daniel-Wei <49779031+Daniel-Wei@users.noreply.github.com>
2023-09-27 01:56:52 +1000

    Create Solution.cs

---
+++ 算法/DP/Type_1/0494-Target_Sum/Solution.cs
@@ -0,0 +1,42 @@
+Time: 92 ms (84.42%), Space: 48.7 MB (20.78%)
+
+public class Solution {
+    public int FindTargetSumWays(int[] nums, int target) {
+        
+        // Basic Solution
+//         List<int> possibleSums = new List<int>(){nums[0], -nums[0]};
+//         for(int i = 1; i < nums.Length; i++){
+//             List<int> curr = new();
+//             possibleSums.ForEach(sum => {
+//                 curr.Add(sum + nums[i]);
+//                 curr.Add(sum - nums[i]);
+//             });
+//             possibleSums = curr; 
+//         }
+        
+//         return possibleSums.Where(s => s == target).Count();
+        
+        
+        int sum = nums.Sum();
+        //edge cases
+        if(target > sum || target < -sum){
+            return 0;
+        }
+        
+        //1-d memo dp
+        //no explicitly saving all possible sums every time
+        int[]memo = new int[sum * 2 + 1];
+        memo[sum] = 1; //represents 0
+        for(int i = 0; i < nums.Length; i++){
+            int[]next = new int[sum * 2 + 1];
+            for(int k = 0; k < sum * 2 + 1; k++){
+                if(memo[k] != 0){
+                    next[k + nums[i]] += memo[k];
+                    next[k - nums[i]] += memo[k];
+                }
+            }
+            memo = next;
+        }
+        return memo[sum + target];
+    }
+}
