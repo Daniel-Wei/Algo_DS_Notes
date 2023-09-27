515ecd59856a3589aecbae589799513c04836038
Daniel-Wei <49779031+Daniel-Wei@users.noreply.github.com>
2023-09-28 01:00:45 +1000

    Create Solution.cs

---
+++ 算法/DP/Type_2/1049-Last_Stone_Weight_ii/Solution.cs
@@ -0,0 +1,19 @@
+Time: 81 ms (64.29%), Space: 40.4 MB (35.71%)
+
+public class Solution {
+    public int LastStoneWeightII(int[] stones) {
+        HashSet<int>[] dp = new HashSet<int>[stones.Length];
+        dp[0] = new HashSet<int>(){stones[0]};
+      
+        for(int i = 1; i < stones.Length; i++){
+            dp[i] = new HashSet<int>();
+            int curr = stones[i];
+            
+            foreach(int sum in dp[i - 1]){
+                dp[i].Add(Math.Abs(curr - sum)); //Add to the group of stones[0]
+                dp[i].Add(Math.Abs(curr + sum)); //Not add to the group of stones[0]
+            };
+        }
+        return dp[stones.Length - 1].Min();
+    }
+}
