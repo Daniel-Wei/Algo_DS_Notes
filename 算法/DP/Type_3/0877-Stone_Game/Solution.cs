38d99493ea8dc82f48347e8adbe19c3db9dfdd99
Daniel-Wei <49779031+Daniel-Wei@users.noreply.github.com>
2023-11-05 02:48:51 +1100

    Create Solution.cs

---
+++ 算法/DP/Type_3/0877-Stone_Game/Solution.cs
@@ -0,0 +1,26 @@
+### Time: 83 ms (82.61%), Space: 44.4 MB (27.54%) 
+
+public class Solution {
+//策略：取first还是last 目的在于使remaining piles 最大可能和最小
+    public bool StoneGame(int[] piles) {
+        int totalSum = piles.Sum();
+        int n = piles.Length;
+        int[, ] dp = new int[n, n];
+        
+        for(int i = 0; i < n; i++){
+            int currSum = 0;
+            for(int j = i; j >=0; j--){
+                currSum += piles[j];
+                
+                if(i == j){
+                    dp[i, i] = piles[i];
+                }else{
+                    //从piles[j]到piles[i]，max = max(total - dp[j+1, i], total - dp[j, i-1])
+                    dp[j, i] = Math.Max(currSum - dp[j+1, i], currSum - dp[j, i-1]);
+                }
+            }
+        }
+        
+        return 2 * dp[1, n-1] < totalSum || 2 * dp[0, n-2] < totalSum;
+    }
+}
