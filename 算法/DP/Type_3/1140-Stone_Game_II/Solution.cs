4d4765aed6e58b0f684a3d46d1a28b735550e6e4
Daniel-Wei <49779031+Daniel-Wei@users.noreply.github.com>
2023-11-06 00:19:30 +1100

    Create Solution.cs

---
+++ 算法/DP/Type_3/1140-Stone_Game_II/Solution.cs
@@ -0,0 +1,46 @@
+## Time: 75 ms (100.00%), Space: 39.3 MB (87.50%)
+
+public class Solution {
+    private int n;
+    private int[ , ] dp;
+    private int[] sums;
+    private int[] piles;
+    
+    public int StoneGameII(int[] piles) {
+        n = piles.Length;
+        dp = new int[n, n];
+        sums = new int[n];
+        piles = piles;
+        
+        sums[n-1] = piles[n-1];
+        for(int i = n-2; i >= 0; i--){
+            sums[i] = sums[i + 1] + piles[i];
+        }
+        
+        return helper(0, 1);
+    }
+    
+    public int helper(int i, int m){
+        
+        // if could get all the remaining piles
+        if(i + m * 2 >= n){
+            return sums[i];
+        }
+        
+        //avoid repeated calculations
+        if(dp[i, m] != 0){
+            return dp[i, m];
+        }
+        
+        //Strategy/ Alice & Bob play optimally: make the next player's files number to be the minimum one
+        int min = int.MaxValue;
+        for(int x = 1; x <= 2 * m; x++){
+            if(i + x < n){
+                 min = Math.Min(min, helper(i+x, Math.Max(x, m)));
+            }
+        }
+        
+        dp[i, m] = sums[i] - min;
+        return dp[i, m];
+    }
+}
