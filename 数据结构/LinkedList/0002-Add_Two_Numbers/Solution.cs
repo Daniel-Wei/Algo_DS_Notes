62435053d3782d4ad558b733d7eddd3b69f60daa
Daniel-Wei <49779031+Daniel-Wei@users.noreply.github.com>
2024-04-01 21:19:48 +1100

    Create Solution.cs

---
+++ 数据结构/LinkedList/0002-Add_Two_Numbers/Solution.cs
@@ -0,0 +1,50 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+// Time: 84 ms (25.43%), Space: 51.5 MB (46.22%)
+
+public class Solution {
+    public ListNode AddTwoNumbers(ListNode l1, ListNode l2) {
+        int extra = 0;
+        
+        ListNode res = new ListNode();
+        ListNode currNode = res;
+        
+        while(l1 != null || l2 != null){
+            if(l1 != null){
+                currNode.val += l1.val;
+            }
+            
+            if(l2 != null){
+                currNode.val += l2.val;
+            }
+            
+            if(currNode.val >= 10){
+                currNode.val %= 10;
+                extra = 1;
+            }else{
+                extra = 0;
+            }
+            
+            l1 = l1 != null ? l1.next : null;
+            l2 = l2 != null? l2.next : null;
+            
+            if(l1 != null || l2 != null || extra == 1){
+                ListNode nextNode = new ListNode(extra);
+                currNode.next = nextNode;
+                currNode = nextNode;
+            }
+        }
+        
+        return res;
+    }
+}
