920c57933637e48b7bf1b987d9b7b5ebc2074618
Daniel-Wei <49779031+Daniel-Wei@users.noreply.github.com>
2024-04-04 01:19:29 +1100

    Create Solution.cs

---
+++ String/0058-Length_Of_Last_Word/Solution.cs
@@ -0,0 +1,17 @@
+// Time: 45 ms (58.42%), Space: 38.1 MB (75.65%)
+
+public class Solution {
+    public int LengthOfLastWord(string s) {
+        s = s.Trim();
+        int res = s.Length;
+        
+        for(int i = s.Length - 1; i >= 0; i--){
+            if(s[i] == ' '){
+                res -= (i + 1);
+                break;
+            }
+        }
+        
+        return res;
+    }
+}
