5e5bcb7706043dcfaedfb47374896bf07b0feb34
Daniel-Wei <49779031+Daniel-Wei@users.noreply.github.com>
2024-04-05 00:15:08 +1100

    Create Solution.cs

---
+++ 算法/BackTracking/0079-Word_Search/0473-Matchsticks_to_Square/Solution.cs
@@ -0,0 +1,45 @@
+// Time: 913 ms (26.41%), Space: 124.3 MB (18.87%)
+
+public class Solution {
+    public bool Makesquare(int[] matchsticks) {
+        if(matchsticks.Sum() % 4 != 0){
+            return false;
+        }
+        
+        int target = matchsticks.Sum() / 4;
+        int[] lengths = [0, 0, 0, 0];
+        
+        return Helper(matchsticks, lengths, target, 0);
+    }
+    
+    private bool Helper(int[] matchsticks, int[]lengths, int target, int index){
+        
+        //base case
+        if(index == matchsticks.Length){
+            return lengths.All(l => l == target);
+        }
+        
+        int curr = matchsticks[index];
+        
+        HashSet<int> distincts = new HashSet<int>();
+        
+        for(int i = 0; i < lengths.Length; i++){
+            int l = lengths[i];
+            
+            if(!distincts.Contains(l) && l + curr <= target){
+                distincts.Add(l);
+                lengths[i] += curr;
+                
+                if(Helper(matchsticks, lengths, target, index + 1)){
+                    return true;
+                }
+                
+                lengths[i] -= curr;
+                
+            }
+        }
+        
+        return false;
+        
+    }
+}
