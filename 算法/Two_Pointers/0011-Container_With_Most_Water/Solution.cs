8df5351c62f19504f3ddaa1549c92b03ee1168fb
Daniel-Wei <49779031+Daniel-Wei@users.noreply.github.com>
2024-04-06 03:09:07 +1100

    Create Solution.cs

---
+++ 算法/Two_Pointers/0011-Container_With_Most_Water/Solution.cs
@@ -0,0 +1,25 @@
+// Time: 252 ms (45.93%), Space: 62.3 MB (34.77%)
+
+public class Solution {
+    public int MaxArea(int[] height) {
+        int l = 0;
+        int r = height.Length - 1;
+        int res= 0;
+        
+        // do not cross each other
+        while(l < r){
+            int curr = Math.Min(height[l], height[r]) * (r - l);
+            
+            res = Math.Max(curr, res);
+            
+            // try to increase the shorter side
+            if(height[l] < height[r]){
+                l++;
+            }else{
+                r--;
+            }
+        }
+        
+        return res;
+    }
+}
