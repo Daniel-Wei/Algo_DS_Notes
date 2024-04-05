679d6c762aecf86de64f08268ea7facdd7747c07
Daniel-Wei <49779031+Daniel-Wei@users.noreply.github.com>
2024-04-06 02:40:02 +1100

    Create Solution.cs

---
+++ 数据结构/Stack/1209-Remove_All_Adjacent_Duplicates_in_String_II/Solution.cs
@@ -0,0 +1,40 @@
+// Time: 64 ms (84.34%), Space: 45.3 MB (46.39%)
+
+public class Solution{
+    public string RemoveDuplicates(string s, int k)
+    {
+        //no need to track specific index of each char
+        //just record latest count of each char
+        Stack<(char ch, int count)> stack = new Stack<(char, int)>();
+        StringBuilder sb = new StringBuilder();
+        
+        foreach(char c in s){
+            if(stack.Any() && stack.Peek().ch == c){
+                var temp = stack.Pop();
+                temp.count += 1;
+                
+                if(temp.count != k){
+                    stack.Push(temp);
+                }
+            }else{
+                stack.Push((c, 1));
+            }
+        }
+        
+        
+        while (stack.Any())
+        {
+            var top = stack.Pop();
+            for (int i = 0; i < top.count; i++)
+            {
+                sb.Append(top.ch);
+            }
+        }
+        
+        char[]chars = sb.ToString().ToCharArray();
+        
+        //FILO
+        Array.Reverse(chars);
+        return new string(chars);
+    }
+}
