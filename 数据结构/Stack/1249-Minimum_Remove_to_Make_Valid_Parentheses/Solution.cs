c244881ef9ec8787e003ef7a8cc17d3318cf7c0b
Daniel-Wei <49779031+Daniel-Wei@users.noreply.github.com>
2024-04-07 20:50:27 +1000

    Create Solution.cs

---
+++ 数据结构/Stack/1249-Minimum_Remove_to_Make_Valid_Parentheses/Solution.cs
@@ -0,0 +1,76 @@
+// Time: 61 ms (81.96%), Space: 47 MB (55.50%)
+
+public class Solution {
+    public string MinRemoveToMakeValid(string s) {
+        
+        // Solution Two: directly construct res, saved time
+        var res = new StringBuilder();
+        var openPos = new Stack<int>();
+        int i = 0;
+        foreach (var c in s)
+            switch (c)
+            {
+                case '(':
+                    res.Append(c);
+                    openPos.Push(i++);
+                    break;
+                case ')':
+                    if (openPos.Any())
+                    {
+                        res.Append(c);
+                        openPos.Pop();
+                        i++;
+                    }
+                    break;
+                default:
+                    res.Append(c);
+                    i++;
+                    break;
+            }
+
+        while (openPos.Any())
+        {
+            var pos = openPos.Pop();
+            res.Remove(pos, 1);
+        }
+
+        return res.ToString();
+        
+        // Solution One: TLE
+//         Stack<int> stack = new Stack<int>();
+//         char[]chars = s.ToCharArray();
+//         string res = "";
+        
+//         // No need to record number of open/closed parenthese
+//         // If current char is closed parenthese, try pop the last open parenthese
+//         for(int i = 0; i < chars.Length; i++){
+//             char ch = chars[i];
+//             if(ch == '('){
+//                 stack.Push(i);
+//             }
+            
+//             if(ch == ')'){
+//                 if(stack.Any()){
+//                     stack.Pop();
+//                 }else{
+//                     chars[i] = '*';
+//                 }
+//             }
+//         }
+        
+        
+//         // Remove remaining open parentheses
+//         // 剪枝：不要在反响重复上面的过程
+//         while(stack.Any()){
+//             chars[stack.Pop()] = '*';
+//         }
+        
+//         for(int i = 0; i < chars.Length; i++){
+//             char curr = chars[i];
+//             if(curr != '*'){
+//                 res += curr;
+//             }
+//         }
+//         return res;
+    }
+}
