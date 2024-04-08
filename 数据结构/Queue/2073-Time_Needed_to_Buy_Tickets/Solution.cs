d0333cf69d5ac4b5c4dcb13a0f71d6da53236aad
Daniel-Wei <49779031+Daniel-Wei@users.noreply.github.com>
2024-04-09 00:00:34 +1000

    Create Solution.cs

---
+++ 数据结构/Queue/2073-Time_Needed_to_Buy_Tickets/Solution.cs
@@ -0,0 +1,31 @@
+// Time: 61 ms (64.71%), Space: 40.8 MB (56.86%)
+
+public class Solution {
+    public int TimeRequiredToBuy(int[] tickets, int k) {
+        Queue<int> q = new Queue<int>(tickets);
+        int t = tickets[k];
+        int counter = 0;
+        int curr = 0;
+        int res = 0;
+        int l = tickets.Length;
+        
+        while(t != 0){
+            curr = q.Dequeue();
+            
+            if(curr > 0){
+                curr -= 1;
+                res += 1;
+            }
+            
+            q.Enqueue(curr);
+            
+            if(counter % l == k){
+                t -= 1;
+            }
+            
+            counter += 1;
+        }
+        
+        return res;
+    }
+}
