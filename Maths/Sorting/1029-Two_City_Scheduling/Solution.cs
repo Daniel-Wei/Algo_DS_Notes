377a5b2e4f1ca2d7929514ead0d6b4da53fb84db
Daniel-Wei <49779031+Daniel-Wei@users.noreply.github.com>
2024-04-09 03:10:02 +1000

    Create Solution.cs

---
+++ Maths/Sorting/1029-Two_City_Scheduling/Solution.cs
@@ -0,0 +1,27 @@
+// Time: 60 ms (81.82%), Space: 40.7 MB (72.73%)
+
+public class Solution {
+    
+    // Tracking exact selections or number of sending to A/B is comsuming and difficuclt
+    // Instead, all sending to A first, then sending half to B with the largest minimisation
+    public int TwoCitySchedCost(int[][] costs) {
+       
+        // All send to A
+        int res = costs.ToList().Sum(c => c[0]);
+        
+        List<int> diff = new List<int>();
+        
+        //Budget diff between sending to A and sending to B
+        foreach(int[]cost in costs){
+            diff.Add(cost[1] - cost[0]);
+        }
+        
+        diff.Sort();
+        
+        for(int i = 0; i < costs.Length / 2; i++){
+            res += diff[i];
+        }
+        
+        return res;
+    }
+}
