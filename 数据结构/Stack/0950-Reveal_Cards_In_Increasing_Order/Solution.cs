41e2f7a5a17acbaa17e37a31e4522035d715bbe0
Daniel-Wei <49779031+Daniel-Wei@users.noreply.github.com>
2024-04-11 00:34:22 +1000

    Create Solution.cs

---
+++ 数据结构/Stack/0950-Reveal_Cards_In_Increasing_Order/Solution.cs
@@ -0,0 +1,27 @@
+// Time: 107 ms (57.89%), Space: 45.9 MB (94.74%)
+
+public class Solution {
+    // Use Queue to reverse the whole game process
+    public int[] DeckRevealedIncreasing(int[] deck) {
+        if(deck.Length == 1){
+            return deck;
+        }
+        
+        Queue<int> q= new Queue<int>();
+        Array.Sort(deck, (a, b) => b.CompareTo(a));
+        q.Enqueue(deck[0]);
+        q.Enqueue(deck[1]);
+        
+        for(int i = 2; i < deck.Length; i++){
+            int curr = q.Peek();
+            q.Dequeue();
+            q.Enqueue(curr);
+            q.Enqueue(deck[i]);
+        }
+        
+        int[]res = q.ToArray();
+        Array.Reverse(res);
+        
+        return res;
+    }
+}
