9b9cfff0e358c12d32c3ba3c80674b72df94f694
Daniel-Wei <49779031+Daniel-Wei@users.noreply.github.com>
2024-04-12 02:06:18 +1000

    Create Solution.cs

---
+++ 数据结构/Stack/0402-Remove_K_Digits/Solution.cs
@@ -0,0 +1,41 @@
+// Time: 304 ms (15.91%), Space: 128.4 MB (9.09%)
+
+public class Solution {
+    public string RemoveKdigits(string num, int k) {
+        Stack<char>s = new Stack<char>();
+        s.Push(num[0]);
+        
+        for(int i = 1; i < num.Length; i++){
+            char curr = num[i];
+            
+            //Push the smaller number to as high digit as possible
+            while(k > 0 && s.Any() && s.Peek() > curr){
+                s.Pop();
+                k --;
+            }
+            
+            // Avoid leading zero
+            // !(!stack.Any() && curr == '0')
+            if(s.Any() || curr != '0'){
+                 s.Push(curr);
+            } 
+        }
+        
+        // In case like (112, 1) should be "11" not "112"
+        while(s.Any() && k > 0){
+            k--;
+            s.Pop();
+        }
+        
+        string res = "0";
+        
+        if(s.Count > 0){
+            res = "";
+            while(s.Any()){
+                res = s.Pop() + res;
+            }
+        }
+        
+        return res;
+    }
+}
