ddd84352d2bb246ee629306cc98484621d9d209a
Daniel-Wei <49779031+Daniel-Wei@users.noreply.github.com>
2024-04-14 22:15:17 +1000

    Create Solution.cs

---
+++ 算法/Two_Pointers/0042-Trapping_Rain_Water/Solution.cs
@@ -0,0 +1,46 @@
+// Time: 85 ms (72.95%), Space: 44.9 MB (81.25%)
+
+class Solution {
+    public int Trap(int[] height) {
+        int l = 0;
+        int r = 1;
+        int res = 0;
+        
+        
+        // From Left to Right
+        // Only count trapped water when right bounder > left bounder
+        while(r < height.Length){
+            if(height[r] <= height[l]){
+                r ++;
+            }else{
+                for(int i = l + 1; i < r; i++){
+                    res += height[l] - height[i];
+                }
+                
+                l = r;
+                r++;
+            }
+        }
+        
+        l = height.Length - 2;
+        r = height.Length - 1;
+        
+        // From Right to Left
+        // Only count when Left Bounder > Right Bounder
+        // Also when Left Bounder == Right Bounder, as we have not considered such cases before
+        while(l >= 0){
+            if(height[l] < height[r]){
+                l--;
+            }else{
+                for(int i = l + 1; i < r; i++){
+                    res += height[r] - height[i];
+                }
+                
+                r = l; 
+                l--;
+            }
+        }
+        
+        return res;
+    }
+}
