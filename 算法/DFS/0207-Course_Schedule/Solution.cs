a3ed40b44eceb1e00323d04e7e990f447ee6dd74
Daniel-Wei <49779031+Daniel-Wei@users.noreply.github.com>
2024-04-24 02:04:25 +1000

    Create Solution.cs

---
+++ 算法/DFS/0207-Course_Schedule/Solution.cs
@@ -0,0 +1,45 @@
+// Time: 99 ms (71.19%), Space: 47.8 MB (74.63%)
+
+public class Solution {
+    public bool CanFinish(int numCourses, int[][] prerequisites) {
+        
+        //With comfirmed number of nodes
+        //Better use Array rather than Dict/HashSet
+        List<int>[] g = new List<int>[numCourses];
+        bool[] visited = new bool[numCourses];
+        bool[] tmpVisited = new bool[numCourses];
+        
+        for(int i = 0; i < numCourses; ++i){
+            g[i] = new List<int>();
+        }
+        
+        for(int i = 0; i < prerequisites.GetLength(0); ++i){
+            g[prerequisites[i][1]].Add(prerequisites[i][0]);
+        }
+        
+        for(int i = 0; i < numCourses; ++i){
+            if(!Dfs(g, visited, tmpVisited, i))
+                return false;
+        }
+        
+        return true;
+    }
+    
+    private bool Dfs(List<int>[] graph, bool[] visited, bool[] tmpVisited, int node){
+        if(tmpVisited[node])
+            return false;
+        if(visited[node])
+            return true;
+        
+        tmpVisited[node] = true;
+        
+        foreach(int n in graph[node]){
+            if(!Dfs(graph, visited, tmpVisited, n))
+                return false;
+        }
+        
+        tmpVisited[node] = false;
+        visited[node] = true;
+        return true;
+    }
+}
