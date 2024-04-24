09e01a4fff4bc54d274d17bedfe5974920ac56dc
Daniel-Wei <49779031+Daniel-Wei@users.noreply.github.com>
2024-04-24 22:19:34 +1000

    Create Solution.cs

---
+++ 算法/DP/Type_1/1137-N-th_Tribonacci_Number/Solution.cs
@@ -0,0 +1,24 @@
+// Time: 18 ms (80.00%), Space: 27 MB (32.76%)
+
+public class Solution {
+    public int Tribonacci(int n) {
+        if(n == 0 || n == 1){
+            return n;
+        }
+        
+        if(n == 2){
+            return 1;
+        }
+        
+        int[] nums = new int[n + 1];
+        nums[0] = 0;
+        nums[1] = 1;
+        nums[2] = 1;
+        
+        for(int i = 3; i < n + 1; i++){
+            nums[i] = nums[i-1] + nums[i-2] + nums[i-3];
+        }
+        
+        return nums[n];
+    }
+}
