e347418b4c52f3e724bafe8caf79fbeab8b6efcf
Daniel-Wei <49779031+Daniel-Wei@users.noreply.github.com>
2024-04-27 16:38:27 +1000

    Create Solution.cs

---
+++ 算法/DP/Type_1/0063-Unique_Paths_II/Solution.cs
@@ -0,0 +1,30 @@
+// Time: 64 ms (53.36%), Space: 40.6 MB (64.08%)
+
+public class Solution {
+    public int UniquePathsWithObstacles(int[][] obstacleGrid) {
+        int n = obstacleGrid.Length;
+        int m = obstacleGrid[0].Length;
+        int[,] dp = new int[n, m];
+        
+        if(obstacleGrid[0][0] == 1){
+            dp[0, 0] = 0;
+        }else{
+            dp[0, 0] = 1;
+        }
+        
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(obstacleGrid[i][j] != 1){
+                    if(i - 1 >= 0){
+                        dp[i,j] += dp[i-1, j];
+                    }
+                    if(j - 1 >= 0){
+                        dp[i, j] += dp[i, j - 1];
+                    }
+                }
+            }
+        }
+        
+        return dp[n-1, m-1];
+    }
+}
