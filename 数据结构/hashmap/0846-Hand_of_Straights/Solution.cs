591bc2a1d761cb2a1781a8df05a62d4614b09bc4
Daniel-Wei <49779031+Daniel-Wei@users.noreply.github.com>
2024-06-10 04:35:39 +1000

    Create Solution.cs

---
+++ 数据结构/hashmap/0846-Hand_of_Straights/Solution.cs
@@ -0,0 +1,34 @@
+// Time: 821 ms (6.95%), Space: 53.1 MB (37.24%)
+
+public class Solution {
+    public bool IsNStraightHand(int[] hand, int groupSize) {
+         
+        Dictionary<int, int> dict = new Dictionary<int, int>();
+        
+        foreach(int num in hand){
+            if(!dict.ContainsKey(num))
+                dict.Add(num, 0);
+            dict[num]++;
+        }
+
+        while(dict.Count >= groupSize){
+            int min = dict.Keys.Min();
+            for(int i = 0; i < groupSize; i++){
+                if(dict.ContainsKey(min)){
+                    dict[min]--;
+                    
+                    if(dict[min] == 0)
+                        dict.Remove(min);
+                    
+                    min++;
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        if(dict.Count > 0)
+            return false;
+        return true;
+    }
+}
