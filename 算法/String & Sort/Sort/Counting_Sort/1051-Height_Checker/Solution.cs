ac01df19a2646600a7d66b59bba14eabe23f6cca
Daniel-Wei <49779031+Daniel-Wei@users.noreply.github.com>
2024-06-10 19:49:51 +1000

    Create Solution.cs

---
+++ 算法/String & Sort/Sort/Counting_Sort/1051-Height_Checker/Solution.cs
@@ -0,0 +1,19 @@
+// Time: 60 ms (64.40%), Space: 40.1 MB (90.40%)
+
+public class Solution {
+    public int HeightChecker(int[] heights) {
+        int[]copy = new int[heights.Length];
+        heights.CopyTo(copy, 0);
+        Array.Sort(heights);
+        
+        int res = 0;
+        
+        for(int i = 0; i < heights.Length; i++){
+            if(heights[i] != copy[i]){
+                res++;
+            }
+        }
+        
+        return res;
+    }
+}
