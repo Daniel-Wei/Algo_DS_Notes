134f522a0860e2da4362d38edac41551ebb82a88
Daniel-Wei <49779031+Daniel-Wei@users.noreply.github.com>
2024-06-17 20:43:42 +1000

    Create Solution.cs

---
+++ 算法/Binary_Search/0069-Sqrt(x)/Solution.cs
@@ -0,0 +1,26 @@
+// Time: 10 ms (99.38%), Space: 26.8 MB (78.20%)
+
+public class Solution {
+    public int MySqrt(int x) {
+        int l = 0;
+        int r = x;
+        int mid = x;
+        
+        while(l <= r){
+            mid = (l + r) / 2;
+            long curr = (long)mid * mid;
+            
+            if(curr > x){
+                r = mid - 1;
+            }else if(curr <= x){
+                l = mid + 1;
+            }
+        }
+        
+        if((long)mid * mid > x){
+            return mid - 1;
+        }else{
+            return mid;
+        }
+    }
+}
