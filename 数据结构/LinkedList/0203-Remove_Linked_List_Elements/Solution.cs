c31390f4e45738648b0d4035cf6b2a3c529d7c71
Daniel-Wei <49779031+Daniel-Wei@users.noreply.github.com>
2024-06-17 21:50:40 +1000

    Create Solution.cs

---
+++ 数据结构/LinkedList/0203-Remove_Linked_List_Elements/Solution.cs
@@ -0,0 +1,31 @@
+// Time: 97 ms (6.06%), Space: 45.2 MB (98.29%)
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode RemoveElements(ListNode head, int val) {
+        ListNode dummy = new ListNode(0, head);
+        ListNode prev = dummy;
+        ListNode curr = head;
+        
+        while(curr != null){
+            if(curr.val == val){
+                prev.next = curr.next;
+            }else{
+                prev = curr;
+            }
+             curr = curr.next;
+        }
+        
+        return dummy.next;
+    }
+}
