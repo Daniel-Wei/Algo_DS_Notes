61c98165119e9055433af3a48f0955417d091541
Daniel-Wei <49779031+Daniel-Wei@users.noreply.github.com>
2024-06-19 01:02:21 +1000

    Create Solution.cs

---
+++ 算法/String & Sort/Sort/Bucket_Sort/Solution.cs
@@ -0,0 +1,33 @@
+// Time: 188 ms (66.25%), Space: 51.2 MB (98.75%) 
+
+public class Solution {
+    public int MaxProfitAssignment(int[] difficulty, int[] profit, int[] worker) {
+        int res = 0;
+        int minD = difficulty.Min();
+        int maxD = difficulty.Max();
+        int maxP = profit.Max();
+        
+        int[]dp = new int[maxD + 1];
+        
+        for(int i = 0; i < profit.Length; i++){
+            // There could be multiple same difficulties with different profits.
+            dp[difficulty[i]] = Math.Max(dp[difficulty[i]], profit[i]);
+        }
+        
+        for(int i = 1; i < dp.Length; i++){
+            dp[i] = Math.Max(dp[i - 1], dp[i]);
+        }
+        
+        foreach(int w in worker){
+            if(w < minD){
+                res += 0;
+            }else if(w > maxD){
+                res += maxP;
+            }else{
+                res += dp[w];
+            }
+        }
+        
+        return res;
+    }
+}
