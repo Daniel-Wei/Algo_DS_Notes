5fc3d99f9f66b2a1e759584d1230d057160334cb
Daniel-Wei <49779031+Daniel-Wei@users.noreply.github.com>
2024-06-21 00:11:11 +1000

    Create Solution.cs

---
+++ 算法/Binary_Search/1552-Magnetic_Force_Between_Two_Balls/Solution.cs
@@ -0,0 +1,43 @@
+// Time: 308 ms (60.71%), Space: 58.1 MB (100.00%)
+
+public class Solution {
+    public int MaxDistance(int[] position, int m) {
+        Array.Sort(position);
+       
+        // low and high are possible forces
+        int low = 1;
+        int high = position[position.Length - 1] - position[0];
+        int res = 0;
+        
+        while(low <= high){
+            int mid = (low + high) / 2;
+            
+            if(CanSearch(position, m, mid)){
+                res = mid;
+                low = mid + 1;
+            }else{
+                high = mid - 1;
+            }
+        }
+        
+        return res;
+    }
+    
+    private bool CanSearch(int[]position, int numOfBalls, int force){
+        int last = position[0];
+        
+        // for example: m = 3
+        // there should be two indexes in the position 
+        // that position[i-1] - position[0] >= force && position[i-2] - position[0] >= force
+        int count = 1;
+        
+        foreach(int p in position){
+            if(p - last >= force){
+                last = p;
+                count++;
+            }
+        }
+       
+        return count >= numOfBalls;
+    }
+}
