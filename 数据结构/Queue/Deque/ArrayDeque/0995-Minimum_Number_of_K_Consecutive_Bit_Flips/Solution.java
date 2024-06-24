6984f710d9290e26068781388dbccd3d34a70169
Daniel-Wei <49779031+Daniel-Wei@users.noreply.github.com>
2024-06-25 02:32:23 +1000

    Create Solution.java

---
+++ 数据结构/Queue/Deque/ArrayDeque/0995-Minimum_Number_of_K_Consecutive_Bit_Flips/Solution.java
@@ -0,0 +1,29 @@
+// Time: 15 ms (29.03%), Space: 57.4 MB (18.71%)
+
+class Solution {
+    public int minKBitFlips(int[] nums, int k) {
+        Deque<Integer> deque = new ArrayDeque<Integer>();
+        int n = nums.length;
+        int res = 0;
+        
+        for(int i = 0; i < n; i++){
+                
+            // keep the sliding window size as k
+            while(deque.size() > 0 && i > deque.peekFirst() + k - 1){
+                deque.pollFirst();
+            }
+            
+            // greedily flip bits whenever there is a 0
+            if((deque.size() + nums[i]) % 2 == 0){
+                if(i + k > n){
+                    return -1;
+                }
+
+                res++;
+                deque.add(i);
+            }
+        }
+        
+        return res;
+    }
+}
