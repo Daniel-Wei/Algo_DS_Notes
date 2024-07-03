5e138c5523be09ce8ff64160ec2e8c30a03ca118
Daniel-Wei <49779031+Daniel-Wei@users.noreply.github.com>
2024-07-03 22:45:41 +1000

    Create Solution.java

---
+++ 算法/Two_Pointers/1509-Minimum_Difference_Between_Largest_and_Smallest_Value_in_Three_Moves/Solution.java
@@ -0,0 +1,26 @@
+// Time: 15 ms (96.06%), Space: 57 MB (64.83%)
+
+class Solution {
+    public int minDifference(int[] nums) {
+        int n = nums.length;
+        if(n <= 4){
+            return 0;
+        }
+        Arrays.sort(nums);
+        
+        int l = 0;
+        int r = n - 4;
+        int count = 0;
+        int res = nums[r] - nums[l];
+
+        while(count < 3){
+            l++;
+            r++;
+            
+            res = Math.min(res, nums[r] - nums[l]);
+            count++;
+        }
+        
+        return res;
+    }
+}
