21e702ac5a8834593905afc12ef90ae07f82cb23
Daniel-Wei <49779031+Daniel-Wei@users.noreply.github.com>
2024-07-05 00:32:23 +1000

    Create Solution.java

---
+++ 算法/Two_Pointers/2181-Merge_Nodes_in_Between_Zeros/Solution.java
@@ -0,0 +1,40 @@
+// Time: 4 ms (100.00%), Space: 81.8 MB (62.80%)
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeNodes(ListNode head) {
+        head = head.next;
+        ListNode p1 = head;
+        ListNode p2 = head.next;
+        
+        
+        while(p2 != null){
+            
+            if(p2.val != 0){
+                p1.val += p2.val;
+            }else{
+                p1.next = p2.next;
+                p1 = p2.next;
+                
+                if(p1 == null){
+                    break;
+                }else{
+                    p2 = p2.next;
+                }
+            }
+            
+            p2 = p2.next;
+        }
+        
+        return head;
+    }
+}
