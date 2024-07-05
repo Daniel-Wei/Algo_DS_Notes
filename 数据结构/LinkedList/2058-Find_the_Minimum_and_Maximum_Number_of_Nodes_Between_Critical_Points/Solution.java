10487c42240c97f33bcf8f3d1f7c313c8625f969
Daniel-Wei <49779031+Daniel-Wei@users.noreply.github.com>
2024-07-06 03:36:00 +1000

    Create Solution.java

---
+++ 数据结构/LinkedList/2058-Find_the_Minimum_and_Maximum_Number_of_Nodes_Between_Critical_Points/Solution.java
@@ -0,0 +1,51 @@
+// Time: 4 ms (100.00%), Space: 60.2 MB (80.22%)
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int[] nodesBetweenCriticalPoints(ListNode head) {
+        int first = -1;
+        int last = -1;
+        int prevIndex = 0;
+        int minDistance = Integer.MAX_VALUE;
+        int index = 1;
+        
+        ListNode curr = head;
+        ListNode next = head.next;
+        
+        while(next != null){
+            if(next.next == null){
+                break;
+            }else{
+                if((curr.val > next.val && next.next.val > next.val) || 
+                   (curr.val < next.val && next.val > next.next.val)){
+                    if(first == -1){
+                        first = index;
+                        prevIndex = index;
+                    }else{
+                        minDistance = Math.min(minDistance, index - prevIndex);
+                        prevIndex = index;
+                        last = index;
+                    }
+                }
+                index++;
+                curr = next;
+                next = next.next;
+            }
+        }
+        
+        if(first == -1 || last == -1){
+            return new int[]{-1, -1};
+        }else{
+            return new int[]{minDistance, last - first};
+        }
+    }
+}
