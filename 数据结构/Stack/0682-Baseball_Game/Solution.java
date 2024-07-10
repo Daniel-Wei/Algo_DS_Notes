1e5eb4177a89a0480328bbb03ffd1e68bbecd45c
Daniel-Wei <49779031+Daniel-Wei@users.noreply.github.com>
2024-07-10 22:52:06 +1000

    Create Solution.java

---
+++ 数据结构/Stack/0682-Baseball_Game/Solution.java
@@ -0,0 +1,38 @@
+// Time: 2 ms (86.61%), Space: 41.6 MB (41.80%)
+
+class Solution {
+    public int calPoints(String[] operations) {
+        Stack<Integer> s = new Stack<Integer>();
+        int last = 0;
+        int secondLast = 0;
+        int next = 0;
+        
+        for(String o : operations){
+            if(o.equals("C")){
+                s.pop();
+            }else if(o.equals("+")){
+                last = s.pop();
+                secondLast = s.pop();
+                next = last + secondLast;
+                s.push(secondLast);
+                s.push(last);
+                s.push(next);
+            }else if(o.equals("D")){
+                last = s.pop();
+                next = last * 2;
+                s.push(last);
+                s.push(next);
+            }else{
+                s.push(Integer.parseInt(o));
+            }
+        }
+        
+        int res = 0;
+        
+        while(s.size() > 0){
+            res += s.pop();
+        }
+        
+        return res;
+    }
+}
