37a795d67a4e5c207f5831801288289c47dda1bc
Daniel-Wei <49779031+Daniel-Wei@users.noreply.github.com>
2024-07-15 22:25:27 +1000

    Create Solution.java

---
+++ 数据结构/hashmap/2196-Create_Binary_Tree_From_Descriptions/Solution.java
@@ -0,0 +1,52 @@
+// Time: 63 ms (79.80%), Space: 55.5 MB (71.52%)
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode createBinaryTree(int[][] descriptions) {
+        HashMap<Integer, TreeNode> m = new HashMap<Integer, TreeNode>();
+        TreeNode root = new TreeNode();
+        Set<Integer> children = new HashSet<Integer>();
+        
+        for(int[] d : descriptions){
+            
+            int parentVal = d[0];
+            int childVal = d[1];
+            children.add(childVal);
+            TreeNode parent = m.getOrDefault(parentVal, new TreeNode(parentVal));
+            TreeNode child = m.getOrDefault(childVal, new TreeNode(childVal));
+            
+            if(d[2] == 1){
+                parent.left = child;
+            }else{
+                parent.right = child;
+            }
+            
+            m.put(parentVal, parent);
+            m.put(childVal, child);
+        }
+        
+        
+        for(Integer key : m.keySet()){
+            if(!children.contains(key)){
+                root = m.get(key);
+                break;
+            }
+        }
+        
+        return root;
+    }
+}
