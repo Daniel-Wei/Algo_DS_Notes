89d4b45c7ddfe435b72c3ed9a46c0d5d736079cd
Daniel-Wei <49779031+Daniel-Wei@users.noreply.github.com>
2024-07-21 03:43:25 +1000

    Create Solution.java

---
+++ Graph/Matrix/1605-Find_Valid_Matrix_Given_Row_and_Column_Sums/Solution.java
@@ -0,0 +1,26 @@
+// Time: 1 ms (100.00%), Space: 53.3 MB (6.77%)
+
+class Solution {
+    public int[][] restoreMatrix(int[] rowSum, int[] colSum) {
+        int M = rowSum.length;
+        int N = colSum.length;
+        int[][] m = new int[M][N];
+        int i = 0;
+        int j = 0;
+        
+        while(i < M && j < N){
+            int curr = Math.min(rowSum[i], colSum[j]);
+            m[i][j] = curr;
+            rowSum[i] -= curr;
+            colSum[j] -= curr;
+            
+            if(rowSum[i] == 0){
+                i++;
+            }else{
+                j++;
+            }
+        }
+        
+        return m;
+    }
+}
