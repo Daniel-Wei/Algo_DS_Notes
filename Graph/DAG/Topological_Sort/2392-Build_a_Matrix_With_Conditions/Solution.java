168c88d11eee7093e99b6be78d808fb139137019
Daniel-Wei <49779031+Daniel-Wei@users.noreply.github.com>
2024-07-22 01:58:26 +1000

    Update Solution.java

--- Graph/DAG/Topological_Sort/2392-Build_a_Matrix_With_Conditions/Solution.java
+++ Graph/DAG/Topological_Sort/2392-Build_a_Matrix_With_Conditions/Solution.java
@@ -5,7 +5,10 @@ class Solution {
         HashMap<Integer, Integer> rowOrdering = new HashMap<Integer, Integer>();
         HashMap<Integer, Integer> colOrdering = new HashMap<Integer, Integer>();
 
+        //Record relationships in row/col conditions
         List<Integer>[] g = new ArrayList[k + 1];
+        
+        //Record which nodes have been visited, to avoid unnecessary repeated DFS search
         boolean[]visited = new boolean[k + 1];
         
         for(int m = 1; m < k + 1; m++){
@@ -20,10 +23,14 @@ class Solution {
         
         for(int j = 1; j <= k; j++){
             i = topologicalSort(visited, j, rowOrdering, k, g, i);
+            
+            //Cyclic path has been detected
             if(i == -99){
                 return new int[0][0];
             }
         }
+        
+        //Repeat same process for col conditions
         
         i = k - 1;
         
@@ -39,11 +46,12 @@ class Solution {
         
         for(int j = 1; j <= k; j++){
             i = topologicalSort(visited, j, colOrdering, k, g, i);
+            
+            //Cyclic path has been detected
             if(i == -99){
                 return new int[0][0];
             }
         }
-        
         
         int[][]matrix = new int[k][k];
         
@@ -61,11 +69,13 @@ class Solution {
             
             if(dfs(node, visited, g, reached, tmpVisited)){
                 for(int m: reached){
+                     // DFS already added nodes in reverse order
                      ordering.put(m, i);
                      i -= 1;
                 }
                 
             }else{
+                //Cyclic path has been detected
                 return -99;
             }
         }
@@ -74,11 +84,13 @@ class Solution {
     }
     
     private boolean dfs(int node, boolean[]visited, List<Integer>[] g, List<Integer>reached, HashSet<Integer> tmpVisited){
+        
+        //Cyclic path has been detected
         if(tmpVisited.contains(node)){
             return false;
         }
         
-        if(visited[node] == false){
+        if(!visited[node]){
             tmpVisited.add(node);
             visited[node] = true;
 
@@ -87,11 +99,15 @@ class Solution {
                     return false;
                 }
             }
-
-            tmpVisited.remove(node);
+            
             reached.add(node);
+
+            // when one node's DFS is done, we removed such node from tmpVisited
+            // because in further DFS search, such node could be visited again: [1,2], [1,3], [3,2]
+            // tmpVisited purpose: make sure no cyclic paths in current DFS path search
+            tmpVisited.remove(node);
         }
-       
+
         return true;
        
     }
