3129d67fe60f494502c27c7d106edcc69b0c60d3
Daniel-Wei <49779031+Daniel-Wei@users.noreply.github.com>
2024-07-23 00:40:32 +1000

    Create Solution.java

---
+++ 数据结构/Pair/2545-Sort_the_Students_by_Their_Kth_Score/Solution.java
@@ -0,0 +1,24 @@
+// Time: 2 ms (92.95%), Space: 50.3 MB (88.80%)
+
+class Solution {
+    public int[][] sortTheStudents(int[][] score, int k) {
+        int s = score.length;
+        int e = score[0].length;
+        
+        PriorityQueue<Pair<Integer, Integer>> pq = new PriorityQueue<>((a, b) -> b.getKey() - a.getKey());
+        
+        for(int i = 0; i < s; i++){
+            pq.add(new Pair<>(score[i][k], i));
+        }
+        
+        int i = 0;
+        int[][]res = new int[s][e];
+        
+        while(pq.size() > 0){
+            res[i] = score[pq.poll().getValue()];
+            i++;
+        }
+        
+        return res;
+    }
+}
