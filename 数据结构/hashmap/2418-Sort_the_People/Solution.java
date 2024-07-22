27c6760588110fd5c34c1ee932f49a6ac1b708a6
Daniel-Wei <49779031+Daniel-Wei@users.noreply.github.com>
2024-07-23 00:25:43 +1000

    Create Solution.java

---
+++ 数据结构/hashmap/2418-Sort_the_People/Solution.java
@@ -0,0 +1,23 @@
+// Time: 7 ms (90.64%), Space: 45.5 MB (18.45%)
+
+class Solution {
+    public String[] sortPeople(String[] names, int[] heights) {
+        HashMap<Integer, Integer> hm = new HashMap<Integer, Integer>();
+        int n = heights.length;
+        
+        for(int i = 0; i < n; i++){
+            hm.put(heights[i], i);
+        }
+        
+        Arrays.sort(heights);
+        int i = n - 1;
+        String[]res = new String[n];
+        
+        for(int h : heights){
+            res[i] = names[hm.get(h)];
+            i--;
+        }
+        
+        return res;
+    }
+}
