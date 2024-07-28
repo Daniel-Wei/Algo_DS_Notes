12d800d5c01308121e8c4dd7dd83347ded4b3389
Daniel-Wei <49779031+Daniel-Wei@users.noreply.github.com>
2024-07-29 01:16:22 +1000

    Create Solution.java

---
+++ Graph/Graph_Theory/Dijkstra/1334-Find_the_City_With_the_Smallest_Number_of_Neighbors_at_a_Threshold_Distance/Solution.java
@@ -0,0 +1,66 @@
+// Time: 28 ms (25.17%), Space: 45.2 MB (8.26%) 
+
+class Solution {
+    public int findTheCity(int n, int[][] edges, int distanceThreshold) {
+        int[][]g = new int[n][n];
+        
+        for(int[]e : edges){
+            g[e[0]][e[1]] = e[2];
+            g[e[1]][e[0]] = e[2];
+        }
+        
+        int res = 0;
+        int count = n;
+        
+        for(int i = 0; i < n; i++){
+            int currCount = dijkstraAlgo(i, g, n, distanceThreshold);
+            
+            if(currCount <= count){
+                count = currCount;
+                res = i;
+            }
+        }
+        
+        return res;
+    }
+    
+    private int dijkstraAlgo(int start, int[][]g, int n, int threshold){
+        PriorityQueue<Pair<Integer, Integer>> pq = new PriorityQueue<Pair<Integer, Integer>>((a, b) -> a.getValue() - b.getValue());
+        
+        int[] dist = new int[n];
+        
+        for(int i = 0; i < n; i++){
+            dist[i] = Integer.MAX_VALUE;
+        }
+        
+        dist[start] = 0;
+        
+        pq.add(new Pair<Integer, Integer>(start, 0));
+        
+        while(pq.size() > 0){
+            Pair<Integer, Integer> curr = pq.poll();
+            if(curr.getValue() <= threshold){
+                int k = curr.getKey();
+                int[]edges = g[k];
+
+
+                for(int i = 0; i < n; i++){
+                    if(edges[i] > 0 && edges[i] + dist[k] < dist[i]){
+                        pq.add(new Pair<Integer, Integer> (i, edges[i] + dist[k]));
+                        dist[i] = edges[i] + dist[k];
+                    }
+                }
+            }
+        }
+        
+        int count = 0;
+        
+        for(int d : dist){
+            if(d <= threshold){
+                count++;
+            }
+        }
+        
+        return count;
+    }
+}
