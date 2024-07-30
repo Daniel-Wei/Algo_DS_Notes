31313bbfbc54a44bc578d30099a0f7d6f90f4d7a
Daniel-Wei <49779031+Daniel-Wei@users.noreply.github.com>
2024-07-31 02:44:03 +1000

    Create Solution.java

---
+++ 算法/DP/Type_2/1653-Minimum_Deletions_to_Make_String_Balanced/Solution.java
@@ -0,0 +1,23 @@
+// Time: 23 ms (69.92%), Space: 45.5 MB (39.55%)
+
+class Solution {
+    public int minimumDeletions(String s) {
+        int n = s.length();
+        int[]memo = new int[n + 1];
+        int b = 0;
+        
+        for(int i = 0; i < n; i++){
+            //memo[i + 1] is for avoiding setting up memo[0]
+            if(s.charAt(i) == 'a'){
+                //1. Delete all previous b's
+                //2. Delete current a
+                memo[i + 1] = Math.min(memo[i]+1, b);
+            }else{
+                b++;
+                memo[i + 1] = memo[i];
+            }
+        }
+        
+        return memo[n];
+    }
+}
