943efae2be31eb121127c87cff36acd39e3cdaa5
Daniel-Wei <49779031+Daniel-Wei@users.noreply.github.com>
2024-08-12 22:38:13 +1000

    Create Solution.java

---
+++ 数据结构/heapq/0703-Kth_Largest_Element_in_a_Stream/Solution.java
@@ -0,0 +1,31 @@
+// Time: 12 ms (97.27%), Space: 46.7 MB (87.97%)
+
+class KthLargest {
+    private PriorityQueue<Integer> pq;
+    private int k;
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        this.pq = new PriorityQueue<Integer>((a, b) -> a - b);
+        
+        for(int num: nums){
+            add(num);
+        }
+    }
+    
+    public int add(int val) {
+        if(this.pq.size() < k){
+            this.pq.offer(val);
+        }else if(this.pq.peek() < val){
+            this.pq.poll();
+            this.pq.add(val);
+        }
+        
+        return this.pq.peek();
+    }
+}
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest obj = new KthLargest(k, nums);
+ * int param_1 = obj.add(val);
+ */
