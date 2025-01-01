a3b3a1425367a2afde886138322025733cbc5d4e
Daniel-Wei <49779031+Daniel-Wei@users.noreply.github.com>
2025-01-01 15:30:18 +1100

    Create Solution.java

---
+++ 算法/Prefix_Sum/1422-Maximum_Score_After_Splitting_a_String/Solution.java
@@ -0,0 +1,40 @@
+// Time: 1 ms, Memory: 41.7 MB
+
+class Solution {
+    public int maxScore(String s) {
+        int len = s.length();
+        int numOfOnes = 0;
+        int score = 0;
+        int tmp = 0;
+
+        if(s.charAt(0) - '0' == 0){
+            tmp = 1;
+            score = 1;
+        }else{
+            tmp = -1;
+            score = -1;
+            numOfOnes++;
+        }
+
+        // 不需要记录右边1的数量 再reversely iterate records 用numOfOnes去减
+        // 直接把正向iterate到的1 当作总和的-1就好
+        // Reverse records 求和不过也就是 leftOnes + numOfRecords - rightsOnes
+        for(int i = 1; i < len - 1; i++){
+            char c = s.charAt(i);
+            if(c - '0' == 0){
+                tmp += 1;
+            }else{
+                tmp -= 1;
+                numOfOnes += 1;
+            }
+
+            score = Math.max(score, tmp);
+        }
+
+        if(s.charAt(len - 1) - '1' == 0){
+            numOfOnes++;
+        }
+
+        return score + numOfOnes;
+    }
+}
